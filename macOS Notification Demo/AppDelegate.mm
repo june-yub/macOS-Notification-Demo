5dcbf947fb0ea9854cb1e09466edc1d9a970fe12
Jason Jeon <john40644064@gmail.com>
2022-08-04 21:48:56 +0900

    initial commit

---
+++ macOS Notification Demo/AppDelegate.mm
@@ -0,0 +1,104 @@
+//
+//  AppDelegate.m
+//  macOS Notification Demo
+//
+//  Created by Jason Jeon on 8/3/22.
+//
+
+#import "AppDelegate.h"
+
+#include <string>
+
+#define NSNOTIFICAION_NAME  @"abcd"
+#define NSDISTRIBUTEDNOTIFICATION_NAME @"efgh"
+
+
+@interface AppDelegate ()
+{
+    
+}
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+
+@implementation AppDelegate
+
+@synthesize m_pButtonPostNotification;
+@synthesize m_pButtonPostDistributedNotification;
+@synthesize m_pTextFieldResult;
+
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+    
+    [m_pButtonPostNotification setTitle:@"Post Notification [NSNotificationCenter]"];
+    [m_pButtonPostDistributedNotification setTitle:@"Post Notification [NSDistributedNotificationCenter"];
+    [m_pTextFieldResult setStringValue:@"initialized"];
+    
+//    [[NSNotificationCenter defaultCenter] addObserver:self
+//                                             selector:@selector(didReceiveNoti:)
+//                                                 name:NSNOTIFICAION_NAME
+//                                               object:nil];
+    [[NSDistributedNotificationCenter defaultCenter] addObserver:self
+                                                        selector:@selector(didReceiveDistributedNoti:)
+                                                            name:NSDISTRIBUTEDNOTIFICATION_NAME
+                                                          object:nil];
+
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
+    // Insert code here to tear down your application
+    
+    [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app
+{
+    return YES;
+}
+
+-(void)didReceiveNoti:(NSNotification *)noti
+{
+    printf("didReceiveNoti:\n");
+    [m_pTextFieldResult setStringValue:@"Notification Received!"];
+}
+
+-(void)didReceiveDistributedNoti:(NSNotification *)noti
+{
+    printf("didReceiveDistributedNoti:\n");
+    [m_pTextFieldResult setStringValue:@"Distributed Notification Received!"];
+}
+
+- (IBAction)onClickPostNotification:(id)sender
+{
+    printf("onClickPostNotification:\n");
+    NSDictionary<NSString *, NSNumber*> *dictionaryUserInfo = @{};
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:NSNOTIFICAION_NAME
+                                                        object:nil
+                                                      userInfo:dictionaryUserInfo];
+}
+- (IBAction)onClickedDistributedNotification:(id)sender
+{
+    NSNotification *notification
+        = [[NSNotification alloc]
+           initWithName:[[[NSBundle mainBundle] infoDictionary]
+                         objectForKey:(NSString *)kCFBundleIdentifierKey]
+           object:nil
+           userInfo:@{@"Application Name" : [[NSRunningApplication currentApplication] localizedName]}];
+        
+        [[NSDistributedNotificationCenter defaultCenter] postNotification:notification];
+    //    printf("onClickedDistributedNotification:\n");
+//    NSDictionary<NSString *, NSNumber*> *dictionaryUserInfo = @{};
+//    [[NSNotificationCenter defaultCenter] postNotificationName:NSDISTRIBUTEDNOTIFICATION_NAME
+//                                                        object:nil
+//                                                      userInfo: dictionaryUserInfo];
+}
+
+
+@end
